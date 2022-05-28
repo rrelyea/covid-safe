9e5e8516647e42bec8ac0b0779b6762ff94d6dda
Rob Relyea <rob@relyeas.net>
2022-05-28 11:40:53 -0700

    add bullet in front of all links. add TOC links at top. Consolidate first few paragraphs. add mask search link. fixes #8. Fixes #12.

--- src/App.js
+++ src/App.js
@@ -15,33 +15,41 @@ function App() {
         <div className="toolkit__wrap">
           <div className="toolkit__intro">
           <h1 className="center">covid-safe</h1>
-            <p>Resources to keep yourself and others safe from COVID-&#8288;19.</p>
-            <p>(Info from <a href='https://covid.gov'>covid.gov</a>, reorganized, and supplemented.)</p>
+            <p>Info from <a href='https://covid.gov'>covid.gov</a>, reorganized, and supplemented by other trusted sites to keep yourself and others safe from COVID-&#8288;19.</p>
+            <p>
+              <b><a href='#preventive-medicines'>Preventive Medicines</a>:</b> <a href="#vaccines">Vaccines</a>, <a href="#immunocompromised">For Immunocompromised</a>{' '}
+              <b><a href='#protective-measures'>Protective Measures</a>:</b> <a href="#masks">Masks</a>, <a href="#ventilation">Ventilation</a>, <a href="#tests">Tests</a>{' '}
+              <b><a href='#have-covid'>Have Covid?</a>:</b> <a href="#test-to-treat">Test-to-Treat</a>, <a href="#treatments">Treatments</a>
+            
+             </p>
           </div>
-          <h2 className="line-title">Preventive Medicines</h2>
+
+          <h2 id="preventive-medicines" className="line-title">Preventive Medicines</h2>
+        
           <div className="toolkit__boxes">
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={syringe} className="small" width="47" height="55" alt="Syringe Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id='vaccines'>
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={syringe} className="small" width="47" height="55" alt="Syringe Icon" />
                   </div>
                   <span>Vaccines</span>
                 </h3>
                 <p>Being <i>"up to date"</i> with your COVID-19 vaccines is the best way to protect against severe illness and hospitalization from COVID-&#8288;19.</p>
-                <p className='lt20 mt20'><a href="https://www.cdc.gov/vaccines/covid-19/downloads/COVID-19-vacc-schedule-at-a-glance-508.pdf"><img src={vaccineSchedule} alt="Image of CDC's At-a-Glance Vaccination Schedules Document" /></a></p>
-                <a href="https://vaccines.gov/search" rel="noopener">Find a vaccine or booster at vaccines.gov</a>
+                &#8729; <a href="https://vaccines.gov/search" rel="noopener">Find a vaccine or booster at vaccines.gov</a>
+                <p>CDC At-a-Glance schedule:</p><p className='lt50 mt20'><a href="https://www.cdc.gov/vaccines/covid-19/downloads/COVID-19-vacc-schedule-at-a-glance-508.pdf"><img src={vaccineSchedule} alt="Image of CDC's At-a-Glance Vaccination Schedules Document" /></a></p>
               </div>
             </div>
+
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={syringe} className="small" width="47" height="55" alt="Syringe Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id="immunocompromised">
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={syringe} className="small" width="47" height="55" alt="Syringe Icon" />
                   </div>
@@ -50,23 +58,24 @@ function App() {
                 <p>
                 The latest CDC guidance for those who are immunocompromised, including where to get preventative treatment.
               </p>
-              <a href="https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/people-with-medical-conditions.html#immunocompromised" rel="noopener">CDC guidance for immunocompromised</a>
+              &#8729; <a href="https://www.cdc.gov/coronavirus/2019-ncov/need-extra-precautions/people-with-medical-conditions.html#immunocompromised" rel="noopener">CDC guidance for immunocompromised</a>
 
-                <p>If you are moderately to severely immunocompromised, for many finding Evusheld isn't as easy as it should be.</p>
-                <a href="https://rrelyea.github.io/evusheld" rel="noopener">Evusheld locator</a>
+              <p>If you are moderately to severely immunocompromised, for many finding Evusheld isn't as easy as it should be.</p>
+              &#8729; <a href="https://rrelyea.github.io/evusheld" rel="noopener">Evusheld locator</a>
                
               </div>
             </div>
           </div>
 
-          <h2 className="line-title">Protective Measures</h2>
+          <h2 id="protective-measures" className="line-title">Protective Measures</h2>
+          
           <div className="toolkit__boxes">
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={masks} width="52" height="31" alt="Mask Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id='masks'>
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={masks} width="52" height="31" alt="Mask Icon" />
                   </div>
@@ -74,54 +83,58 @@ function App() {
                 </h3>
                 <p>Wear a mask to protect yourself and others when you go indoors in public. We recommend N95 masks, as they are much more protective.</p>
                 <p>Free N95 masks are available at many local pharmacies and healthcare providers. Check with stores in your area to see where you can find free masks.</p>
-                <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/masks.html" rel="noopener">Learn more about masks and where to get them</a>
+                &#8729; <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/masks.html" rel="noopener">Learn more about masks and where to get them</a><br/>
+                &#8729; <a href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/free-masks.html" rel="noopener">Free mask locator</a>
               </div>
             </div>
+
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={ventilation} width="38" height="38" alt="Ventilation Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id='ventilation'>
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={ventilation} width="38" height="38" alt="Ventilation Icon" />
                   </div>
                   <span>Ventilation</span>
                 </h3>
                 <p>Staying home with only members of your household is the best way to keep SARS-CoV-2 (the virus that causes COVID-19) particles out of your home. However, if a visitor needs to be in your home, improving ventilation (air flow) can help prevent virus particles from accumulating in the air in your home.</p>
-                <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/improving-ventilation-home.html" rel="noopener">Improve Ventilation in Your Home</a>
+                &#8729; <a href="https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/improving-ventilation-home.html" rel="noopener">Improve Ventilation in Your Home</a>
               </div>
             </div>
+
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={tests} width="32" height="42" alt="Beaker Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id="tests">
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={tests} width="32" height="42" alt="Beaker Icon" />
                   </div>
-                  <span>Testing</span>
+                  <span>Tests</span>
                 </h3>
                 <p>COVID tests can help you know if you have COVID-&#8288;19 when you have symptoms, have been exposed, or are about to meet up with others.</p>
-                <a href="https://covid.gov/tests" rel="noopener">Order free tests</a>
+                &#8729; <a href="https://covid.gov/tests" rel="noopener">Order free tests</a>
 
                 <p>
                   The latest guidance on using at-home tests, what to do with the results, and how to get at-home tests.
                 </p>
-                <a href="https://www.cdc.gov/coronavirus/2019-ncov/testing/self-testing.html" rel="noopener">Learn more about at-home testing</a>
+                &#8729; <a href="https://www.cdc.gov/coronavirus/2019-ncov/testing/self-testing.html" rel="noopener">Learn more about at-home testing</a>
               </div>
             </div>
           </div>
 
-          <h2 className="line-title">Have Covid?</h2>
+          <h2 id='have-covid' className="line-title">Have Covid?</h2>
+
           <div className="toolkit__boxes">
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={treatment} width="38" height="38" alt="Medical Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id='test-to-treat'>
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={treatment} width="38" height="38" alt="Medical Icon" />
                   </div>
@@ -129,15 +142,16 @@ function App() {
                 </h3>
                 <p>Treatments for COVID-&#8288;19 are now widely available. If you test positive for COVID-19, talk to a doctor as soon as possible about <a href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/treatments-for-severe-illness.html" className="usa-link usa-link--external" rel="noopener">treatment options</a>.</p>
                 <p>The Test-to-Treat program is one easy way to get treatment. Test-to-Treat locations will give you a test and treatment.</p>
-                <a href="https://COVID-&#8288;19-test-to-treat-locator-dhhs.hub.arcgis.com" rel="noopener">Find a Test-to-Treat location near you</a>
+                &#8729; <a href="https://COVID-&#8288;19-test-to-treat-locator-dhhs.hub.arcgis.com" rel="noopener">Find a Test-to-Treat location near you</a>
               </div>
             </div>
+
             <div className="toolkit__box">
               <div className="toolkit__box__icon toolkit__desktop__icon">
                 <img src={treatment} width="38" height="38" alt="Medical Icon" />
               </div>
               <div className="toolkit__box__content">
-                <h3>
+                <h3 id='treatments'>
                   <div className="toolkit__box__icon toolkit__mobile__icon">
                     <img src={treatment} width="38" height="38" alt="Medical Icon" />
                   </div>
@@ -145,9 +159,9 @@ function App() {
                 </h3>
                 <p>Treatments for COVID-&#8288;19 are now widely available. If you test positive for COVID-19, talk to a doctor as soon as possible about <a href="https://www.cdc.gov/coronavirus/2019-ncov/your-health/treatments-for-severe-illness.html" className="usa-link usa-link--external" rel="noopener">treatment options</a>.</p>
                 <p>Once you get a prescription, sometimes it helps to know which pharmacies have these in stock:</p>
-                <a href="https://rrelyea.github.io/paxlovid" rel="noopener">Paxlovid locator</a><br/>
-                <a href="https://rrelyea.github.io/bebtelovimab" rel="noopener">Bebtelovimab locator</a><br/> 
-                <a href="https://rrelyea.github.io/lagevrio" rel="noopener">Molnuripavir (Lagevrio) locator</a><br/>
+                &#8729; <a href="https://rrelyea.github.io/paxlovid" rel="noopener">Paxlovid locator</a><br/>
+                &#8729; <a href="https://rrelyea.github.io/bebtelovimab" rel="noopener">Bebtelovimab locator</a><br/> 
+                &#8729; <a href="https://rrelyea.github.io/lagevrio" rel="noopener">Molnuripavir (Lagevrio) locator</a><br/>
               </div>
             </div>
           </div>
